6a5382fd1bbe6e43cf6df1619ec056a9acb2ce2c
Soner Yuksel <grkmyksl@gmail.com>
2021-04-06 13:03:22 -0400

    Adding VPN Shortcut and donation

--- Client/Application/Delegates/AppDelegate.swift
+++ Client/Application/Delegates/AppDelegate.swift
@@ -648,6 +648,11 @@ extension AppDelegate {
                     type: .clearBrowsingHistory, using: browserViewController)
                 
                 return true
+            case ActivityType.enableBraveVPN.identifier:
+                ActivityShortcutManager.shared.performShortcutActivity(
+                    type: .enableBraveVPN, using: browserViewController)
+                
+                return true
             default:
                 break
         }
